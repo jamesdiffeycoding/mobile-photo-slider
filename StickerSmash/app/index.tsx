e5b558130e5b2fd8e0248d6bfff42fe92dda7f21
Sara Thampi <saraannthampi@gmail.com>
2024-10-24 11:57:39 +0100

    index and about page

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router'
+
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={
+        styles.container
+      }
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> Home Screen</Text>
+      <Link href="/about" style={styles.button}>Go To About Screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  }, 
+});
